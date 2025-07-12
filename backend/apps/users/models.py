27a4088504032c81e8a8fcac816f0eac0b3baff0
Khoetskiy Sergey <khoetskiy@gmail.com>
2025-07-12 21:50:37 +0200

    delete comments for users/models

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -3,7 +3,3 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     """Пользовательская модель пользователя, расширяющая AbstractUser."""
-
-    # class Meta:
-    #     verbose_name = 'пользователь'
-    #     verbose_name_plural = 'пользователи'
