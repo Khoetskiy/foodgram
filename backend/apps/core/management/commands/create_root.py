493eb83ddb1668ff717beb583cd075cf230db2d7
Khoetskiy Sergey <khoetskiy@gmail.com>
2025-07-20 18:43:01 +0200

    Add custom  commands: - create_root - import_csv

---
+++ backend/apps/core/management/commands/create_root.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+from django.db import IntegrityError
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = 'Создание супер-пользователя'
+
+    def handle(self, *args, **kwargs):
+        username = 'root'
+        email = 'root@mail.com'
+        password = 'root'
+        try:
+            User.objects.create_superuser(
+                username=username, email=email, password=password
+            )
+        except IntegrityError as e:
+            self.stderr.write(
+                self.style.ERROR(
+                    f'Ошибка: {e}\nПользователь с такими данными уже существует!'
+                )
+            )
+        except (ValueError, TypeError) as e:
+            self.stderr.write(
+                self.style.ERROR(
+                    f'Ошибка: {e}\nДанные некорректны!'
+                )
+            )
+        else:
+            msg = 'Создан супер-пользователь с данными:'
+            data = (
+                f'  username: {username}\n  email: {email}\n  password: {password}'
+            )
+            self.stdout.write(self.style.SUCCESS(msg))
+            self.stdout.write(self.style.MIGRATE_LABEL(data))
---
+++ backend/apps/core/management/commands/import_csv.py
@@ -0,0 +1,109 @@
+import csv
+
+from django.contrib.auth import get_user_model
+from django.core.management import BaseCommand
+from django.db import IntegrityError
+
+from apps.cart.models import Cart, CartItem
+from apps.recipes.models import (
+    Ingredient,
+    MeasurementUnit,
+    Recipe,
+    RecipeIngredient,
+    Tag,
+)
+from apps.users.models import Favorite
+
+User = get_user_model()
+
+MODEL_CLASS_MAP = {
+    'user': User,
+    'measurementunit': MeasurementUnit,
+    'ingredient': Ingredient,
+    'recipe': Recipe,
+    'tag': Tag,
+    'recipeingredient': RecipeIngredient,
+    'recipetag': Recipe.tags.through,
+    'cart': Cart,
+    'cartitem': CartItem,
+    'favorite': Favorite,
+    'favoriterecipes': Favorite.recipes.through
+}
+
+
+class Command(BaseCommand):
+    help = 'Загрузка данных в БД из CSV в зависимости от модели.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('model_name', type=str, help='Имя модели')
+        parser.add_argument('file_path', type=str, help='Путь к CSV-файлу')
+
+    def handle(self, *args, **kwargs):
+        model_name = kwargs['model_name']
+        file_path = kwargs['file_path']
+
+        if model_name not in MODEL_CLASS_MAP:
+            self.stderr.write(
+                self.style.ERROR(
+                    f'Импорт для модели "{model_name.capitalize()}" '
+                    'не поддерживается!'
+                )
+            )
+            return
+
+        try:
+            with open(file_path, encoding='utf-8', newline='') as f:
+                reader = csv.DictReader(f)
+                model_class = MODEL_CLASS_MAP[model_name]
+                self.import_data(reader, model_class)
+
+        except FileNotFoundError:
+            self.stderr.write(
+                self.style.ERROR(f'Файл "{file_path}" не найден')
+            )
+        except csv.Error as e:
+            self.stderr.write(self.style.ERROR(f'Ошибка при чтении CSV: {e}'))
+        else:
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f'Импорт данных для модели {model_name} прошел успешно!'
+                )
+            )
+
+    def import_data(self, reader: csv.DictReader, modelclass):
+        """Импортирует данные из CSV в указанную модель базы данных."""
+        bulk_objects = []
+        for row in reader:
+            filtered_data = self.convert_data_types(row)
+
+            try:
+                obj = modelclass(**filtered_data)
+                bulk_objects.append(obj)
+            except ValueError:
+                self.stderr.write(self.style.WARNING(
+                    f'Некорректные данные в строке: {row}'
+                ))
+
+        try:
+            if bulk_objects:
+                modelclass.objects.bulk_create(bulk_objects)
+                self.stdout.write(self.style.SUCCESS(
+                    f'Успешно загружено {len(bulk_objects)} '
+                    f'объектов(а) для модели "{modelclass.__name__}".'
+                ))
+            else:
+                self.stdout.write(self.style.NOTICE(
+                    'Не было загружено ни одного объекта.'
+                ))
+        except IntegrityError as e:
+            self.stderr.write(self.style.ERROR(f'Ошибка IntegrityError: {e}.'))
+            self.stderr.write(self.style.ERROR(f'Строка: {row}.'))
+
+
+
+    def convert_data_types(self, row: dict) -> dict:
+        """Преобразует данные из CSV в нужные типы."""
+        return {
+            field: int(value) if value.isdigit() else value
+            for field, value in row.items()
+        }
