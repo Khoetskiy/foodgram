746ab3c5d1a65b8c6dc2c070e064e31a5c364b5d
Khoetskiy Sergey <khoetskiy@gmail.com>
2025-07-24 00:29:58 +0200

    Add new func in text unitils and change __init__

--- backend/apps/core/utils/__init__.py
+++ backend/apps/core/utils/__init__.py
@@ -19,11 +19,12 @@
 from .files import generate_unique_filename, get_safe_extension
 from .html import render_html_list_block
 from .slug import append_number_to_slug, create_slug, parse_slug_number
-from .text import is_cyrillic, translate_text, truncate_text
+from .text import capitalize_name, is_cyrillic, translate_text, truncate_text
 from .time import format_duration_time
 
 __all__ = [
     'append_number_to_slug',
+    'capitalize_name',
     'create_slug',
     'format_duration_time',
     'generate_unique_filename',
