2f137d1e7881abc54469a6aa5a90668933e0215d
Khoetskiy Sergey <khoetskiy@gmail.com>
2025-08-02 22:46:26 +0200

    Add new util for files - decode_base64_image

--- backend/apps/core/utils/files.py
+++ backend/apps/core/utils/files.py
@@ -3,6 +3,10 @@ import logging
 from pathlib import Path
 from uuid import uuid4
 
+import base64
+from uuid import uuid4
+from django.core.files.base import ContentFile
+
 from apps.core.constants import (
     ALLOWED_EXTENSIONS,
     DEFAULT_EXT,
@@ -46,3 +50,26 @@ def generate_unique_filename(ext: str) -> str:
         str: уникальное название файла.
     """
     return f'{uuid4().hex[:UUID_FILENAME_LENGTH]}{ext}'
+
+
+def decode_base64_image(data: str):
+    """
+    Декодирует строку формата data:image/png;base64,<...> в ContentFile.
+
+    Args:
+        data (str): Строка c изображением в формате data:image/...;base64,...
+
+    Returns:
+        ContentFile: Объект для сохранения изображения.
+
+    Raises:
+        ValueError: Если строка не может быть декодирована.
+    """
+    try:
+        mimetype, imgstr = data.split(';base64,')
+        ext = mimetype.split('/')[-1]
+        filename = generate_unique_filename(f'.{ext}')
+        return ContentFile(base64.b64decode(imgstr), filename)
+    except Exception as e:
+        msg = 'Некорректные данные изображения.'
+        raise ValueError(msg) from e
