367a73589178db2b639a5e5868355e5d92c97842
Khoetskiy Sergey <khoetskiy@gmail.com>
2025-08-02 22:49:15 +0200

    Change root urls: - Add api path - Add api-auth path while DEBUG = True

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,11 +6,20 @@ import config.settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('apps.api.urls')),
 ]
+
 
 if config.settings.DEBUG:
     import debug_toolbar
+
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
     urlpatterns += static(
         config.settings.MEDIA_URL, document_root=config.settings.MEDIA_ROOT
+    )
+    urlpatterns += (
+        path(
+            'api-auth/',
+            include('rest_framework.urls', namespace='rest_framework'),
+        ),  # FIXME: Для SessionAuthentication. Отключить на проде?
     )
