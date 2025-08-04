1ac5c6a480a5d6fa652ea73f40c61c57fc9a09ae
Khoetskiy Sergey <khoetskiy@gmail.com>
2025-08-04 21:30:04 +0200

    Add new permission IsAuthorOrReadOnly

--- backend/apps/api/permissions.py
+++ backend/apps/api/permissions.py
@@ -12,3 +12,17 @@ class IsAdminOrReadOnly(BasePermission):
 class DenyAll(BasePermission):
     def has_permission(self, request, view):
         return False
+
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+    # FIXME: Можно ли оптимизировать  в 1 метод?
