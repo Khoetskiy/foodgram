6061dc0404d4c6a92be509e82cd87b7a1d72dd0c
Khoetskiy Sergey <khoetskiy@gmail.com>
2025-08-06 17:01:21 +0200

    - Refactor urls, delete unused urls and imports - Delete djoser_auth urlpatterns

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -1,54 +1,28 @@
-from django.urls import include, path, re_path
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
 
-from apps.api.routers import MyCustomRouter
 from apps.api.views import (
     CustomUserViewSet,
-    IngredientApiList,
-    IngredientAPIUpdate,
-    IngredientApiView,
-    IngredientDetailAPIView,
     IngredientViewSet,
-    RecipeApiView,
-    RecipeListView,
+    RecipeViewSet,
     TagViewSet,
-    UserViewSet,
-    manage_avatar,
-    RecipeViewSet
 )
 
 app_name = 'api_v1'
 
 router_v1 = DefaultRouter()
 router_v1.register(r'users', CustomUserViewSet, basename='users')
-# router_v1.register(r'users', UserViewSet, basename='users')
 router_v1.register('tags', TagViewSet, basename='tags')
 router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
 router_v1.register('recipes', RecipeViewSet, basename='recipes')
 
 
-
-
-# djoser_auth = [
-#     # re_path(r'^auth/', include('djoser.urls')),
-#     re_path(r'^auth/', include('djoser.urls.authtoken')),
-# ]
-
-
 v1_patterns = [
     path('', include(router_v1.urls)),
     path('auth/', include('djoser.urls.authtoken')),
-    path('users/me/avatar/', manage_avatar, name='manage_avatar'),
 ]
 
 
 urlpatterns = [
     path('v1/', include(v1_patterns)),
 ]
-
-# TODO: Сделать структуру путей
