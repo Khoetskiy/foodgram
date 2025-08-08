abedc5acb448b21a777a3b06babb4238bc8a5c84
Khoetskiy Sergey <khoetskiy@gmail.com>
2025-08-08 18:00:55 +0200

    Delete unused permission class

--- backend/apps/api/permissions.py
+++ backend/apps/api/permissions.py
@@ -1,18 +1,6 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsAdminOrReadOnly(BasePermission):
-    """
-    Разрешает только безопасные методы всем пользователям.
-    Изменяющие методы доступны только администратору.
-    """
-
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS or (
-            request.user and request.user.is_staff
-        )
-
-
 class DenyAll(BasePermission):
     """Полный запрет доступа для всех пользователей и всех методов."""
 
@@ -31,5 +19,3 @@ class IsAuthorOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or obj.author == request.user
-
-# FIXME: Есть ли неиспользуемые permissions? Ели да, то удалить
