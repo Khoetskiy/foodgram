9e39c9116608852d070f0d9a8a05b7373e1f7621
Khoetskiy Sergey <khoetskiy@gmail.com>
2025-08-09 20:41:29 +0200

    Change id to pk

--- backend/apps/recipes/views.py
+++ backend/apps/recipes/views.py
@@ -6,4 +6,4 @@ from apps.recipes.models import Recipe
 def redirect_to_recipe(request, short_code):
     """Перенаправляет c короткой ссылки на страницу рецепта."""
     recipe = get_object_or_404(Recipe, short_code=short_code)
-    return redirect(f'/recipes/{recipe.id}/')
+    return redirect(f'/recipes/{recipe.pk}/')
