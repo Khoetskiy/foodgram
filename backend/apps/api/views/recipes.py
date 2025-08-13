e8d2c150c200bc55ce7d8f2ed8fb5f2308383050
Khoetskiy Sergey <khoetskiy@gmail.com>
2025-08-13 18:26:13 +0200

    Delete unused import

--- backend/apps/api/views/recipes.py
+++ backend/apps/api/views/recipes.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, status, viewsets
+from rest_framework import status, viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
